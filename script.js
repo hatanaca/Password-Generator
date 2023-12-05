1cc0134e82c72248a8cc4dbebdf888eb681f10da
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2023-12-05 13:36:17 -0300

    Finalizado

---
+++ script.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById("password")
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%¨&*()_+~|}{[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassWord(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)]
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+    password += number[Math.floor(Math.random() * number.length)]
+    password += symbol[Math.floor(Math.random() * symbol.length)]
+
+    while(lenght > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)]
+    }
+passwordBox.value = password;
+}
+
+function copyPassword() {
+    const copy = passwordBox.value;
+    navigator.clipboard.writeText(copy);
+  }
